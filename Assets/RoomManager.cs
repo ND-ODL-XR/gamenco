ab5b1a0d453b5747ad8fb5a14fe9aada558b2e5b
NDODL <sophiazh@stanford.edu>
2024-07-01 14:46:39 -0400

    UI fix & half-working keyboard

---
+++ Assets/RoomManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomManager : MonoBehaviour
+{
+
+    public void OnCreateRoom() { 
+        Debug.Log("Create Room");
+    }
+
+    public void OnJoinRoom()
+    {
+        Debug.Log("Join Room");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
