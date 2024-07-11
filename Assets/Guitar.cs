2f402a60a6dd7353923c4ae80226caa7d2ed3e50
NDODL <sophiazh@stanford.edu>
2024-07-11 13:44:34 -0400

    guitar strum pattern vs metronome detection

--- Assets/Guitar.cs
+++ Assets/Guitar.cs
@@ -1,15 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Guitar : MonoBehaviour
 {
     [SerializeField] private AudioSource[] notes;
+    [SerializeField] private Metronome metronome;
+    [SerializeField] private TMPro.TextMeshProUGUI guitarText;
+
     private bool colliding = false;
     public int currentNote = 0;
+    [SerializeField] float cooldown;
+    private float cooldownTimer;
+
+    public sbyte[] beats;
+    public bool playedCurrentBeat = false;
 
     private void Update()
     {
+        cooldownTimer -= Time.deltaTime;
+        if (cooldownTimer <= 0)
+        {
+            cooldownTimer = cooldown;
+            currentNote = 0;
+        }
         colliding = false;
     }
 
@@ -18,13 +33,28 @@ public class Guitar : MonoBehaviour
         if (colliding) return;
         colliding = true;
 
-        if (other.CompareTag("GuitarPick")) { 
+        if (other.CompareTag("GuitarPick")) {
             notes[currentNote].Play();
             currentNote++;
+
+            playedCurrentBeat = true;
+
             if (currentNote >= notes.Length)
             {
                 currentNote = 0;
             }
         }
     }
+
+    public void Fail() { 
+        guitarText.color = Color.red;
+        guitarText.text = "Incorrect";
+    }
+
+    public void Succeed()
+    {
+        guitarText.color = Color.green;
+        guitarText.text = "Correct";
+    }   
+
 }
--- Assets/Metronome.cs
+++ Assets/Metronome.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Metronome : MonoBehaviour
 {
-    [SerializeField] private float beatInterval = 1.5f;
+    // Interval between HALF beats
+    [SerializeField] private float beatInterval;
+    [SerializeField] private float errorMargin;
+    [SerializeField] private TMPro.TextMeshProUGUI beatText;
+    [SerializeField] private Guitar guitar;
+
     private AudioSource audioSource;
     private float beatTimer;
+    public int beatCount;
   
     // Start is called before the first frame update
     void Start()
@@ -19,10 +26,38 @@ public class Metronome : MonoBehaviour
     void Update()
     {
         beatTimer -= Time.deltaTime;
+
         if (beatTimer <= 0)
         {
-            audioSource.Play();
+            if (beatCount == 0)
+            {
+                audioSource.pitch = 3f;
+            }
+            else
+            {
+                audioSource.pitch = 1f;
+            }
+            StartCoroutine(PlayNextBeat());
+            beatCount = (beatCount + 1) % 4;
             beatTimer = beatInterval;
+        }
+    }
+
+    // Coroutine to play the next beat after errorMargin/2 seconds
+    public IEnumerator PlayNextBeat()
+    {
+        int beat = beatCount + 1;
+        yield return new WaitForSeconds(errorMargin / 2);
+        beatText.text = beat.ToString();
+        audioSource.Play();
+        yield return new WaitForSeconds(errorMargin / 2);
+        if (guitar.beats[beat - 1] == 1 && !guitar.playedCurrentBeat || guitar.beats[beat - 1] == 0 && guitar.playedCurrentBeat)
+        {
+            guitar.Fail();
+        } else
+        {
+            guitar.playedCurrentBeat = false;
+            guitar.Succeed();
         }
     }
 }
