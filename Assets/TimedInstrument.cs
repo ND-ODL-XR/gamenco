69ca854a4266c734d3e210088fd61b90076b4899
NDODL <sophiazh@stanford.edu>
2024-08-08 12:58:42 -0400

    some documentation

--- Assets/TimedInstrument.cs
+++ Assets/TimedInstrument.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using UnityEngine.UI;
 using Unity.Netcode;
 
+// Class that represents an instrument that plays notes on a beat e.g. castanets, guitar
+// and judges if it's being played in the right sequence
 
 public abstract class TimedInstrument : NetworkBehaviour
 {
@@ -19,12 +21,14 @@ public abstract class TimedInstrument : NetworkBehaviour
     public bool successfulSequence = true;
     public bool disabled = false;
 
+    // On play note, request to play note on server
     public virtual void PlayNote() {
         PlayNoteServerRpc();
     }
 
     [ServerRpc(RequireOwnership = false)]
     public virtual void PlayNoteServerRpc(ServerRpcParams serverRpcParams = default) {
+        // If the player is the one allowed to play the instrument, play the note across all clients
         if (serverRpcParams.Receive.SenderClientId == allowedPlayerId && !disabled)
         {
             playedCurrentBeat = true;
@@ -32,15 +36,18 @@ public abstract class TimedInstrument : NetworkBehaviour
         }
     }
 
+    // Play the note on each client
     [ClientRpc]
     public virtual void PlayAudioClientRpc() { 
         audioSource.Play();
     }
 
+    // Check if a note has been played on the beat
     // Beat is 1-indexed
     public virtual void OnBeatEnd(int beat) {
         if (disabled) return;
 
+        // If the note was played on the wrong beat, or if the note wasn't played on the right beat, it's incorrect
         if ((beats[beat - 1] == 1 && !playedCurrentBeat) || (beats[beat - 1] == 0 && playedCurrentBeat))
         {
             changeTextClientRpc(Color.red, "Inorrecto.");
@@ -74,7 +81,7 @@ public abstract class TimedInstrument : NetworkBehaviour
 
     public virtual void PlayLoopingNote() {
         // Modded since the beat count is 1 ahead of the actual beat
-        // TODO: Fix this
+        // TODO: Fix this; it doesn't play the right sequence
         if (beats[(metronome.beatCount.Value + 1) % 4] == 1)
         {
             PlayAudioClientRpc();
